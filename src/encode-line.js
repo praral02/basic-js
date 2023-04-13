2f8f3e647dacc45cec79b38fd56277922df8d937
praral02 <prakopenka.artsiom@student.ehu.lt>
2023-04-13 22:20:54 +0300

    fas

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  str = str.split('')
+  let res = []
+  for(let i = 0; i <= str.length; i++){
+    let count = 0;
+    i = 0
+    for (let j = 0; j < str.length; j++) {
+      while(str[i] === str[j]){
+        count += 1
+        j+=1
+      } 
+      if(count == 0){count += 1}
+      break    
+    }
+   if(count > 1){
+      res.push(count + str[i])
+    }else{res.push(str[i])}   
+    str.splice(0,count)
+
+  }
+  return res.join('')
 }
 
 module.exports = {
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  return arr.sort((a,b) => {
+    if(a == -1 || b == -1){
+      return 1
+    }else{ a - b}
+  })
 }
 
 module.exports = {
